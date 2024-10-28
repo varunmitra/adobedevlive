0d24c08e2bfdd0bf27ad2a12a14949c870930ce8
Varun Mitra <vmitra@adobe.com>
2024-10-28 16:00:08 -0400

    replaceing header and footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,59 +1,28 @@
-/* eslint-disable object-curly-newline, function-paren-newline */
-import { div, img, a } from '../../scripts/dom-helpers.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The header block element
+ */
 
 export default async function decorate(block) {
-  const footer = await fetch('/footer.plain.html');
+  const navPath = window.wknd.demoConfig.demoBase || '';
 
-  if (footer.ok) {
-    const htmlText = await footer.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlText, 'text/html');
+  const resp = await fetch(`${navPath}/footer.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  if (resp.ok) {
+    block.textContent = '';
 
-    const $iconRow = div({ class: 'icon-row' },
-      a({ class: 'logo', href: '/', 'aria-label': 'Visit Home Page' }, img({
-        src: '/icons/hubble-homes-logo.svg',
-        width: '110',
-        height: '56',
-        alt: 'Hubble Homes, LLC',
-      })),
-      div(
-        a({ class: 'btn facebook', href: 'https://www.facebook.com/hubblehomes', target: '_blank', 'aria-label': 'Visit us on Facebook' }),
-        a({ class: 'btn youtube', href: 'https://www.youtube.com/channel/UC6IB9iRD8FRnR-aNCZR8PLw', target: '_blank', 'aria-label': 'Visit us on YouTube' }),
-        a({ class: 'btn pinterest', href: 'https://www.pinterest.com/hubblehomesid/', target: '_blank', 'aria-label': 'Visit us on Pinterest' }),
-        a({ class: 'btn instagram', href: 'https://www.instagram.com/hubblehomes/', target: '_blank', 'aria-label': 'Visit us on Instagram' }),
-        a({ class: 'btn news', href: 'https://www.hubblehomes.com/news', target: '_blank', 'aria-label': 'Visit our News' }),
-        a({ class: 'btn hud', href: 'https://www.hud.gov/program_offices/fair_housing_equal_opp', target: '_blank', 'aria-label': 'Visit HUD' }),
-      ),
-    );
+    const html = await resp.text();
+    const footer = document.createElement('div');
+    footer.innerHTML = html;
+    await decorateIcons(footer);
 
-    const $top = div({ class: 'top' }, div());
-    const $bottom = div({ class: 'bottom' }, $iconRow);
-
-    // define sections and map to them
-    const sections = ['col-1', 'col-2', 'col-3', 'terms'];
-    const sectionsHTML = Array.from(doc.body.children);
-
-    sections.forEach((section, row) => {
-      const html = sectionsHTML[row];
-      html.classList.add(section);
-
-      if (html) {
-        if (row >= 0 && row <= 2) {
-          html.querySelectorAll('p > a').forEach((btn) => btn.classList.add('btn'));
-          $top.firstChild.append(html);
-        }
-
-        // bottom row
-        if (row === 3) $bottom.append(html);
-      } else {
-        // eslint-disable-next-line no-console
-        console.log('Section not found for index', row);
-      }
-    });
-
-    block.replaceWith($top, $bottom);
-  } else {
-    // eslint-disable-next-line no-console
-    console.error('Failed to fetch fragment:', footer.statusText);
+    const classes = ['brand', 'nav', 'follow', 'disc'];
+    let f = footer.firstElementChild;
+    while (f && classes.length) {
+      f.classList.add(classes.shift());
+      f = f.nextElementSibling;
+    }
+    block.append(footer);
   }
 }
--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,218 +1,68 @@
-import {
-  a, div, img, input, label, nav, span, waitForElement,
-} from '../../scripts/dom-helpers.js';
-import {
-  getCommunitiesSheet,
-  getStaffSheet,
-  getModelsSheet,
-  getInventorySheet,
-  loadWorkbook,
-  getHomePlansSheet, getCitySheet,
-} from '../../scripts/workbook.js';
-import {
-  formatCommunities,
-  formatStaff,
-  formatModels,
-  formatInventory,
-  formatHomePlans, formatCities, handleSearchNav,
-} from '../../scripts/search-helper.js';
-import { debounce } from '../../scripts/utils.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
 
-const $body = document.body;
+/**
+ * collapses all open nav sections
+ * @param {Element} sections The container element
+ */
 
-function closeDropdown($nav) {
-  const $on = $nav.querySelector('.on');
-  if ($on) $on.classList.remove('on');
-}
-
-async function buildNav() {
-  const fetchNav = await fetch('/nav/nav.plain.html');
-  const navHTML = await fetchNav.text();
-  const $nav = nav();
-  $nav.innerHTML = navHTML;
-
-  if (navHTML) {
-    const $header = document.querySelector('header .header');
-
-    // top level dropdowns
-    const l1LIs = $nav.querySelectorAll('div > ul > li');
-    l1LIs.forEach((li) => {
-      li.addEventListener('click', () => {
-        const isOn = li.classList.contains('on');
-        if (!isOn) closeDropdown($nav);
-        li.classList.toggle('on');
-      });
-    });
-
-    $body.addEventListener('click', (e) => {
-      if (!$nav.contains(e.target)) closeDropdown($nav);
-    });
-
-    // Remove any text nodes that are direct children of <ul> elements
-    $nav.querySelectorAll('ul').forEach((ul) => {
-      ul.childNodes.forEach((node) => {
-        if (node.nodeType === Node.TEXT_NODE) {
-          node.remove();
-        }
-      });
-    });
-
-    $header.append($nav);
-  }
-}
-
-async function setupAutocomplete() {
-  const searchInput = await waitForElement('#navSearch');
-
-  const tabToSearchNav = (e) => {
-    if (e.key === 'ArrowDown') {
-      const firstItem = document.querySelector('#autocomplete-list .search-item');
-      if (firstItem) {
-        searchInput.blur();
-        const list = document.querySelector('#autocomplete-list');
-        list.focus();
-        list.dispatchEvent(new Event('focus'));
-        handleSearchNav(e);
-      }
-    }
-  };
-
-  searchInput.addEventListener('keydown', tabToSearchNav);
-
-  const autocompleteList = await waitForElement('#autocomplete-list');
-  autocompleteList.addEventListener(
-    'focusin',
-    () => autocompleteList.addEventListener('keydown', handleSearchNav),
-  );
-  autocompleteList.addEventListener(
-    'focusout',
-    () => autocompleteList.removeEventListener('keydown', handleSearchNav),
-  );
-  autocompleteList.addEventListener('mouseover', () => {
-    // remove any active elements
-    const activeItem = document.querySelector('.search-item.active');
-    if (activeItem) activeItem.classList.remove('active');
-    // remove any elements with focus
-    const focusedItem = document.querySelector('.search-item:focus');
-    if (focusedItem) focusedItem.blur();
-  });
-
-  const communityResult = await getCommunitiesSheet('data');
-  const staffResult = await getStaffSheet();
-  const modelResult = await getModelsSheet('data');
-  const homePlans = await getHomePlansSheet('data');
-  const inventoryResult = await getInventorySheet('data');
-  const regionsResult = await getCitySheet('data');
-
-  const communityData = formatCommunities(communityResult);
-  const staffData = formatStaff(staffResult);
-  const modelData = formatModels(modelResult);
-  const inventoryData = formatInventory(inventoryResult);
-  const homePlanData = formatHomePlans(homePlans);
-  const cityData = formatCities(regionsResult);
-
-  const allSuggestions = [
-    ...communityData.sort((c1, c2) => c1.display.localeCompare(c2.display)),
-    ...staffData.sort((s1, s2) => s1.display.localeCompare(s2.display)),
-    ...modelData.sort((m1, m2) => m1.display.localeCompare(m2.display)),
-    ...inventoryData.sort((i1, i2) => i1.display.localeCompare(i2.display)),
-    ...homePlanData.sort((p1, p2) => p1.display.localeCompare(p2.display)),
-    ...cityData.sort((c1, c2) => c1.display.localeCompare(c2.display)),
-  ];
-
-  const handleInput = debounce(() => {
-    const value = searchInput.value.toLowerCase();
-    autocompleteList.innerHTML = '';
-
-    if (value.length < 2) return;
-
-    const filteredSuggestions = allSuggestions
-      .filter((item) => item.display.toLowerCase().includes(value));
-
-    filteredSuggestions.forEach((suggestion) => {
-      autocompleteList.append(
-        div({ class: 'search-item', tabIndex: '-1' }, a({
-          class: 'search-item-link',
-          tabIndex: '-1',
-          href: suggestion.path,
-          onclick: () => {
-            autocompleteList.innerHTML = '';
-            searchInput.value = suggestion.value;
-          },
-        }, suggestion.display)),
-      );
-    });
-  }, 200);
-
-  searchInput.addEventListener('input', handleInput);
-
-  document.addEventListener('click', (e) => {
-    if (e.target !== searchInput) {
-      autocompleteList.innerHTML = '';
-    }
+function collapseAllNavSections(sections) {
+  sections.querySelectorAll('.nav-sections > ul > li').forEach((section) => {
+    section.setAttribute('aria-expanded', 'false');
   });
 }
+
+/**
+ * decorates the header, mainly the nav
+ * @param {Element} block The header block element
+ */
 
 export default async function decorate(block) {
-  await loadWorkbook();
+  // fetch nav content
+  const navPath = window.wknd.demoConfig.demoBase || '';
 
-  block.innerHTML = '';
+  const resp = await fetch(`${navPath}/nav.plain.html`, window.location.pathname.endsWith('/nav') ? { cache: 'reload' } : {});
+  if (resp.ok) {
+    const html = await resp.text();
 
-  const $logo = a(
-    {
-      id: 'logo',
-      href: '/',
-      'aria-label': 'Visit Home Page',
-    },
-    img({
-      src: '/icons/hubble-homes-logo.svg',
-      width: '110',
-      height: '56',
-      alt: 'Hubble Homes, LLC',
-    }),
-  );
+    // decorate nav DOM
+    const nav = document.createElement('nav');
+    nav.innerHTML = html;
+    decorateIcons(nav);
 
-  const $search = div(
-    { id: 'search' },
-    label(
-      {
-        class: 'sr-only',
-        for: 'navSearch',
-      },
-      'Type plan, city, community, phrase or MLS',
-    ),
-    div(
-      { class: 'search-icon' },
-      img({
-        src: '/icons/search.svg',
-        height: 25,
-        width: 25,
-        alt: 'search',
-      }),
-    ),
-    input({
-      type: 'text',
-      name: 'navSearch',
-      id: 'navSearch',
-      placeholder: 'Type plan, city, community, phrase or MLS#',
-    }),
-    div({ id: 'autocomplete-list', class: 'autocomplete-items' }),
-  );
-  const $bgrBtn = div({ class: 'bgr-btn' }, span(), span(), span());
-  $bgrBtn.addEventListener('click', () => {
-    const navTransitionTime = 600;
-    if (!$body.classList.contains('mobile-nav-open')) {
-      $body.classList.add('mobile-nav-open');
-      setTimeout(() => {
-        $body.classList.add('done');
-      }, navTransitionTime);
-    } else {
-      $body.classList.remove('mobile-nav-open', 'done');
+    const classes = ['brand', 'sections', 'tools'];
+    classes.forEach((e, j) => {
+      const section = nav.children[j];
+      if (section) section.classList.add(`nav-${e}`);
+    });
+
+    const navSections = [...nav.children][1];
+    if (navSections) {
+      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+        navSection.addEventListener('click', () => {
+          const expanded = navSection.getAttribute('aria-expanded') === 'true';
+          collapseAllNavSections(navSections);
+          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        });
+      });
     }
-  });
 
-  buildNav();
-  setupAutocomplete();
-
-  block.append($logo, $search, $bgrBtn);
+    // hamburger for mobile
+    const hamburger = document.createElement('div');
+    hamburger.classList.add('nav-hamburger');
+    hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
+    hamburger.addEventListener('click', () => {
+      const expanded = nav.getAttribute('aria-expanded') === 'true';
+      document.body.style.overflowY = expanded ? '' : 'hidden';
+      nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+    });
+    const topBar = document.createElement('div');
+    topBar.classList.add('header-topbar');
+    block.prepend(topBar);
+    topBar.innerHTML = '<div>Sign In</div><div class="header-markets"><span class="icon icon-flag-us"></span>EN-US<span class="header-chevron-down"></span></div>';
+    nav.prepend(hamburger);
+    nav.setAttribute('aria-expanded', 'false');
+    decorateIcons(block);
+    block.append(nav);
+  }
 }
