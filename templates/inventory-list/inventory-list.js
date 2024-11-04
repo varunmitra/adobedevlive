e6756ac8671bf7ce478b22654fc9926827d6269f
Varun Mitra <vmitra@adobe.com>
2024-11-04 15:03:15 -0500

    Update inventory-list.js

--- templates/inventory-list/inventory-list.js
+++ templates/inventory-list/inventory-list.js
@@ -4,12 +4,12 @@ import { div, button } from '../../scripts/dom-helpers.js';
 import { loadRates } from '../../scripts/mortgage.js';
 
 let startIndex = 0;
-let endIndex = 0;
+let endIndex;
 const limit = 8;
 
 async function displayCards(inventoryHomes, fragment) {
   endIndex = startIndex + limit;
-  window.hh.current.inventory = inventoryHomes;
+  window.hh.current.inventory = inventoryHomes.slice(startIndex, endIndex);
   //window.hh.current.inventory = inventoryHomes
   const modelsBlock = buildBlock('cards', []);
   modelsBlock.classList.add('inventory');
@@ -22,21 +22,21 @@ async function displayCards(inventoryHomes, fragment) {
 
 export default async function decorate(doc) {
   await loadRates();
-  let observer;
   const fragment = doc.querySelector('.fragment-wrapper');
   fragment.classList.add('disclaimer');
   const inventoryHomes = await getInventorySheet('data');
   const filteredInventory = inventoryHomes.filter((home) => home.status === 'Under Construction');
   const inventorySize = filteredInventory.length;
-  await displayCards(inventoryHomes, fragment);
-  /*observer = new IntersectionObserver((entries) => {
-    entries.forEach(async (entry) => {
-      if (entry.isIntersecting && inventorySize >= endIndex) {
-        startIndex = endIndex;
-        displayCards(inventoryHomes, fragment);
-      }
-      if (endIndex >= inventorySize) observer.disconnect();
-    });
+  const loadMoreBtn = button({ class: 'load-more-btn' }, 'See More');
+  const loadMore = div({ class: 'load-more' }, loadMoreBtn);
+  await displayCards(filteredInventory, fragment);
+ loadMoreBtn.addEventListener(('click'), async () => {
+    startIndex = endIndex;
+    endIndex += limit;
+    await displayCards(filteredInventory, loadMore);
+    if (inventorySize <= endIndex) {
+      loadMore.classList.add('hidden');
+    }
   });
-  observer.observe(fragment);*/
+  fragment.insertAdjacentElement('beforebegin', loadMore);
 }
